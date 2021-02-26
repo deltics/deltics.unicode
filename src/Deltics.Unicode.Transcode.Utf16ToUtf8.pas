d769c4fd43c10668225f43e80192ac507da13b7c
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-27 12:14:07 +1300

    refactor: More meaningful enum names

--- src/Deltics.Unicode.Transcode.Utf16ToUtf8.pas
+++ src/Deltics.Unicode.Transcode.Utf16ToUtf8.pas
@@ -25,45 +25,45 @@ implementation
                          var aUtf8: PUtf8Char;
                          var aUtf8Count: Integer);
   type
-    TState = (BMP, LoSurrogate);
+    TState = (ExpectingCharOrHiSurrogate, ExpectingLoSurrogate);
   var
     state: TState;
     hi: WideChar;
     code: CodePoint;
   begin
     hi    := #$0000;
-    state := BMP;
+    state := ExpectingCharOrHiSurrogate;
 
     while (aUtf16Count > 0) and (aUtf8Count > 0) do
     begin
       case state of
-        BMP         : begin
-                        if Unicode.IsHiSurrogate(aUtf16^) then
-                        begin
-                          hi    := aUtf16^;
-                          state := LoSurrogate;
-                        end
-//                      else if StrictEncoding and Unicode.IsLoSurrogate(aUtf16^) then
-//                        raise EInvalidEncoding.Create('Orphan Low Surrogate');
-                        else
-                          Unicode.CodepointToUtf8(Codepoint(aUtf16^), aUtf8, aUtf8Count);
+        ExpectingCharOrHiSurrogate  : begin
+                                       if Unicode.IsHiSurrogate(aUtf16^) then
+                                       begin
+                                         hi    := aUtf16^;
+                                         state := ExpectingLoSurrogate;
+                                       end
+//                                     else if StrictEncoding and Unicode.IsLoSurrogate(aUtf16^) then
+//                                       raise EInvalidEncoding.Create('Orphan Low Surrogate');
+                                       else
+                                         Unicode.CodepointToUtf8(Codepoint(aUtf16^), aUtf8, aUtf8Count);
 
-                        Inc(aUtf16);
-                        Dec(aUtf16Count);
-                      end;
+                                       Inc(aUtf16);
+                                       Dec(aUtf16Count);
+                                     end;
 
-        LoSurrogate : if Unicode.IsLoSurrogate(aUtf16^) then
-                      begin
-                        code := Unicode.SurrogatesToCodepoint(hi, aUtf16^);
-                        Unicode.CodepointToUtf8(code, aUtf8, aUtf8Count);
+        ExpectingLoSurrogate        : if Unicode.IsLoSurrogate(aUtf16^) then
+                                      begin
+                                        code := Unicode.SurrogatesToCodepoint(hi, aUtf16^);
+                                        Unicode.CodepointToUtf8(code, aUtf8, aUtf8Count);
 
-                        Inc(aUtf16);
-                        Dec(aUtf16Count);
+                                        Inc(aUtf16);
+                                        Dec(aUtf16Count);
 
-                        state := BMP;
-                      end;
-//                      else if StrictEncoding then
-//                        raise EInvalidEncoding.Create('Orphan High Surrogate');
+                                        state := ExpectingCharOrHiSurrogate;
+                                      end;
+//                                    else if StrictEncoding then
+//                                      raise EInvalidEncoding.Create('Orphan High Surrogate');
       end;
     end;
   end;
