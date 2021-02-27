806fadc6bbeb206864bb0b478a4bf5d35b41372b
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-02-28 12:13:12 +1300

    test: Tests for Index and Json escapes

---
+++ tests/Test.Escapes.pas
@@ -0,0 +1,80 @@
+
+  unit Test.Escapes;
+
+
+interface
+
+  uses
+    Deltics.Smoketest;
+
+
+  type
+    Escapes = class(TTest)
+      procedure UtilsAnsiHex;
+      procedure UtilsWideHex;
+      procedure Index;
+      procedure Json;
+    end;
+
+
+
+implementation
+
+  uses
+    Deltics.Unicode,
+    Deltics.Unicode.Utils;
+
+
+{ Escapes ---------------------------------------------------------------------------------------- }
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  procedure Escapes.UtilsAnsiHex;
+  begin
+    Test('AnsiHex(#$004a)').Assert(AnsiHex($004a, 4, TRUE)).Equals('004A');
+    Test('AnsiHex(#$804a)').Assert(AnsiHex($804a, 4, FALSE)).Equals('804a');
+    Test('AnsiHex($10000)').Assert(AnsiHex($10000, 5, FALSE)).Equals('10000');
+    Test('AnsiHex($100000)').Assert(AnsiHex($100000, 6, FALSE)).Equals('100000');
+    Test('AnsiHex($1000000)').Assert(AnsiHex($1000000, 7, FALSE)).Equals('1000000');
+    Test('AnsiHex($10000000)').Assert(AnsiHex($10000000, 8, FALSE)).Equals('10000000');
+    Test('AnsiHex($faedb4c7)').Assert(AnsiHex($faedb4c7, 8, FALSE)).Equals('faedb4c7');
+    Test('AnsiHex($faedb4c7)').Assert(AnsiHex($faedb4c7, 8, TRUE)).Equals('FAEDB4C7');
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  procedure Escapes.UtilsWideHex;
+  begin
+    Test('WideHex(#$004a)').Assert(WideHex($004a, 4, TRUE)).Equals('004A');
+    Test('WideHex(#$804a)').Assert(WideHex($804a, 4, FALSE)).Equals('804a');
+    Test('WideHex($10000)').Assert(WideHex($10000, 5, FALSE)).Equals('10000');
+    Test('WideHex($100000)').Assert(WideHex($100000, 6, FALSE)).Equals('100000');
+    Test('WideHex($1000000)').Assert(WideHex($1000000, 7, FALSE)).Equals('1000000');
+    Test('WideHex($10000000)').Assert(WideHex($10000000, 8, FALSE)).Equals('10000000');
+    Test('WideHex($faedb4c7)').Assert(WideHex($faedb4c7, 8, FALSE)).Equals('faedb4c7');
+    Test('WideHex($faedb4c7)').Assert(WideHex($faedb4c7, 8, TRUE)).Equals('FAEDB4C7');
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  procedure Escapes.Index;
+  begin
+    Test('Index(#$0041)').Assert(Unicode.Index(#$0041)).Equals('U+0041');
+    Test('Index(#$dc00)').Assert(Unicode.Index(#$dc00)).Equals('U+DC00');
+
+    Test('Index($10000)').Assert(Unicode.Index($10000)).Equals('U+10000');
+  end;
+
+
+  { - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - }
+  procedure Escapes.Json;
+  begin
+    Test('Json(#$004a)').Assert(Unicode.Json(#$004a)).Equals('\u004a');
+    Test('Json(#$804a)').Assert(Unicode.Json(#$804a)).Equals('\u804a');
+    Test('Json($10000)').Assert(Unicode.Json($10000)).Equals('\ud800\udc00');
+    Test('Json($100000)').Assert(Unicode.Json($100000)).Equals('\udbc0\udc00');
+  end;
+
+
+
+
+end.
--- tests/test.dpr
+++ tests/test.dpr
@@ -7,15 +7,20 @@ uses
   Deltics.Smoketest,
   Deltics.Unicode in '..\src\Deltics.Unicode.pas',
   Deltics.Unicode.Bom in '..\src\Deltics.Unicode.Bom.pas',
-  Deltics.Unicode.Types in '..\src\Deltics.Unicode.Types.pas',
+  Deltics.Unicode.Class_ in '..\src\Deltics.Unicode.Class_.pas',
   Deltics.Unicode.Exceptions in '..\src\Deltics.Unicode.Exceptions.pas',
+  Deltics.Unicode.Transcode.CodepointToUtf8 in '..\src\Deltics.Unicode.Transcode.CodepointToUtf8.pas',
+  Deltics.Unicode.Transcode.Utf8ToCodepoint in '..\src\Deltics.Unicode.Transcode.Utf8ToCodepoint.pas',
   Deltics.Unicode.Transcode.Utf8ToUtf16 in '..\src\Deltics.Unicode.Transcode.Utf8ToUtf16.pas',
   Deltics.Unicode.Transcode.Utf16ToUtf8 in '..\src\Deltics.Unicode.Transcode.Utf16ToUtf8.pas',
-  Deltics.Unicode.Class_ in '..\src\Deltics.Unicode.Class_.pas',
-  Deltics.Unicode.Transcode.Utf8ToCodepoint in '..\src\Deltics.Unicode.Transcode.Utf8ToCodepoint.pas',
-  Deltics.Unicode.Transcode.CodepointToUtf8 in '..\src\Deltics.Unicode.Transcode.CodepointToUtf8.pas',
-  Test.Unicode in 'Test.Unicode.pas';
+  Deltics.Unicode.Types in '..\src\Deltics.Unicode.Types.pas',
+  Test.Unicode in 'Test.Unicode.pas',
+  Test.Escapes in 'Test.Escapes.pas',
+  Deltics.Unicode.Escape.Index in '..\src\Deltics.Unicode.Escape.Index.pas',
+  Deltics.Unicode.Utils in '..\src\Deltics.Unicode.Utils.pas',
+  Deltics.Unicode.Escape.Json in '..\src\Deltics.Unicode.Escape.Json.pas';
 
 begin
+  TestRun.Test(Escapes);
   TestRun.Test(Utils);
 end.
