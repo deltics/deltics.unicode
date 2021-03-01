23d36604047556d3199111f2ceb9bad4eacc1639
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-03-01 16:04:44 +1300

    feat: AsciiChar as subrange type and comprehensive char/string/arrays

--- src/Deltics.Unicode.Types.pas
+++ src/Deltics.Unicode.Types.pas
@@ -7,20 +7,6 @@
 interface
 
   type
-  {$ifdef UNICODE}
-    Utf8String    = System.Utf8String;
-    UnicodeString = System.UnicodeString;
-  {$else}
-    Utf8String    = type AnsiString;
-    UnicodeString = type WideString;
-  {$endif}
-
-    Utf8Char      = type AnsiChar;
-    PUtf8Char     = ^Utf8Char;
-
-    AsciiString   = type Utf8String;
-    AsciiChar     = type Utf8Char;
-
   // A Codepoint is a natively expressed (LE on Windows) 32-bit value
   //  identifying a unique Unicode codepoint and is an alias for the Utf32Char type.
 
@@ -28,9 +14,39 @@ interface
     PCodepoint  = ^Codepoint;
 
 
+  // Strings
+
+  {$ifdef UNICODE}
+    Utf8String    = System.Utf8String;
+    UnicodeString = System.UnicodeString;
+  {$else}
+    Utf8String    = type AnsiString;
+    UnicodeString = type WideString;
+  {$endif}
+    AsciiString   = type Utf8String;
+
+
+  // Chars
+
+    AsciiChar     = $0..$127;
+    Utf8Char      = type AnsiChar;
+    Utf16Char     = WideChar;
+    Utf32Char     = type Codepoint;
+
+    PAsciiChar    = ^AsciiChar;
+    PUtf8Char     = ^Utf8Char;
+    PUtf16Char    = ^Utf16Char;
+    PUtf32Char    = ^Utf32Char;
+
+
   // Dynamic arrays of Character types
 
+    AnsiArray       = array of AnsiChar;
+    AsciiArray      = array of AsciiChar;
+    CharArray       = array of Char;
     Utf8Array       = array of Utf8Char;
+    Utf16Array      = array of Utf16Char;
+    Utf32Array      = array of Utf32Char;
     WideCharArray   = array of WideChar;
     CodepointArray  = array of Codepoint;
 
@@ -38,9 +54,11 @@ interface
 
     AnsiStringArray     = array of AnsiString;
     AsciiStringArray    = array of AsciiString;
+    StringArray         = array of String;
     UnicodeStringArray  = array of UnicodeString;
     Utf8StringArray     = array of Utf8String;
     WideStringArray     = array of WideString;
+
 
   // Character/Codepoint Escape Encoders
 
