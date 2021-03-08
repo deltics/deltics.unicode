e825fb065d851c145a6600a83472f34e2208853a
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-03-08 15:54:02 +1300

    feat: Utf16Be <-> Utf8 encoding

--- src/Deltics.Unicode.Transcode.Utf16ToUtf8.pas
+++ src/Deltics.Unicode.Transcode.Utf16ToUtf8.pas
@@ -10,7 +10,8 @@ interface
     Deltics.Unicode.Types;
 
 
-  procedure _Utf16ToUtf8(var aUtf16: PWideChar; var aUtf16Count: Integer; var aUtf8: PUtf8Char; var aUtf8Count: Integer);
+  procedure _Utf16BeToUtf8(var aUtf16: PWideChar; var aUtf16Count: Integer; var aUtf8: PUtf8Char; var aUtf8Count: Integer);
+  procedure _Utf16LeToUtf8(var aUtf16: PWideChar; var aUtf16Count: Integer; var aUtf8: PUtf8Char; var aUtf8Count: Integer);
 
 
 
@@ -20,10 +21,62 @@ implementation
     Deltics.Unicode;
 
 
-  procedure _Utf16ToUtf8(var aUtf16: PWideChar;
+  procedure _Utf16BeToUtf8(var aUtf16: PWideChar;
                          var aUtf16Count: Integer;
                          var aUtf8: PUtf8Char;
                          var aUtf8Count: Integer);
+  type
+    TState = (ExpectingCharOrHiSurrogate, ExpectingLoSurrogate);
+  var
+    state: TState;
+    ch: WideChar;
+    hi: WideChar;
+    code: CodePoint;
+  begin
+    hi    := #$0000;
+    state := ExpectingCharOrHiSurrogate;
+
+    while (aUtf16Count > 0) and (aUtf8Count > 0) do
+    begin
+      ch := WideChar(((Word(aUtf16^) and $ff00) shr 8) or ((Word(aUtf16^) and $ff) shl 8));
+
+      case state of
+        ExpectingCharOrHiSurrogate  : begin
+                                       if Unicode.IsHiSurrogate(ch) then
+                                       begin
+                                         hi    := ch;
+                                         state := ExpectingLoSurrogate;
+                                       end
+//                                     else if StrictEncoding and Unicode.IsLoSurrogate(aUtf16^) then
+//                                       raise EInvalidEncoding.Create('Orphan Low Surrogate');
+                                       else
+                                         Unicode.CodepointToUtf8(Codepoint(ch), aUtf8, aUtf8Count);
+
+                                       Inc(aUtf16);
+                                       Dec(aUtf16Count);
+                                     end;
+
+        ExpectingLoSurrogate        : if Unicode.IsLoSurrogate(ch) then
+                                      begin
+                                        code := Unicode.SurrogatesToCodepoint(hi, ch);
+                                        Unicode.CodepointToUtf8(code, aUtf8, aUtf8Count);
+
+                                        Inc(aUtf16);
+                                        Dec(aUtf16Count);
+
+                                        state := ExpectingCharOrHiSurrogate;
+                                      end;
+//                                    else if StrictEncoding then
+//                                      raise EInvalidEncoding.Create('Orphan High Surrogate');
+      end;
+    end;
+  end;
+
+
+  procedure _Utf16LeToUtf8(var aUtf16: PWideChar;
+                           var aUtf16Count: Integer;
+                           var aUtf8: PUtf8Char;
+                           var aUtf8Count: Integer);
   type
     TState = (ExpectingCharOrHiSurrogate, ExpectingLoSurrogate);
   var
@@ -67,8 +120,6 @@ implementation
       end;
     end;
   end;
-
-
 
 
 
--- src/Deltics.Unicode.Transcode.Utf8ToUtf16.pas
+++ src/Deltics.Unicode.Transcode.Utf8ToUtf16.pas
@@ -10,7 +10,8 @@ interface
     Deltics.Unicode.Types;
 
 
-  procedure _Utf8ToUtf16(var aUtf8: PUtf8Char; var aUtf8Count: Integer; var aUtf16: PWideChar; var aUtf16Count: Integer);
+  procedure _Utf8ToUtf16Be(var aUtf8: PUtf8Char; var aUtf8Count: Integer; var aUtf16: PWideChar; var aUtf16Count: Integer);
+  procedure _Utf8ToUtf16Le(var aUtf8: PUtf8Char; var aUtf8Count: Integer; var aUtf16: PWideChar; var aUtf16Count: Integer);
 
 
 
@@ -20,10 +21,10 @@ implementation
     Deltics.Unicode;
 
 
-  procedure _Utf8ToUtf16(var aUtf8: PUtf8Char;
-                         var aUtf8Count: Integer;
-                         var aUtf16: PWideChar;
-                         var aUtf16Count: Integer);
+  procedure _Utf8ToUtf16Be(var aUtf8: PUtf8Char;
+                           var aUtf8Count: Integer;
+                           var aUtf16: PWideChar;
+                           var aUtf16Count: Integer);
   var
     code: Codepoint;
     hi, lo: WideChar;
@@ -35,7 +36,43 @@ implementation
       if code > $ffff then
       begin
         if aUtf16Count = 1 then
-          raise EMoreData.Create('%s requires a surrogate pair but only 1 widechar remains available in the destination buffer', [Unicode.Index(code)]);
+          raise EMoreData.Create('%s requires a surrogate pair but only 1 widechar remains available in the destination buffer', [Unicode.Ref(code)]);
+
+        Unicode.CodepointToSurrogates(code, hi, lo);
+
+        aUtf16^ := WideChar(((Word(hi) and $ff00) shr 8) or ((Word(hi) and $00ff) shl 8));
+        Inc(aUtf16);
+        aUtf16^ := WideChar(((Word(lo) and $ff00) shr 8) or ((Word(lo) and $00ff) shl 8));
+        Inc(aUtf16);
+
+        Dec(aUtf16Count, 2);
+      end
+      else
+      begin
+        aUtf16^ := WideChar(((Word(code) and $ff00) shr 8) or ((Word(code) and $00ff) shl 8));
+        Inc(aUtf16);
+        Dec(aUtf16Count);
+      end;
+    end;
+  end;
+
+
+  procedure _Utf8ToUtf16Le(var aUtf8: PUtf8Char;
+                           var aUtf8Count: Integer;
+                           var aUtf16: PWideChar;
+                           var aUtf16Count: Integer);
+  var
+    code: Codepoint;
+    hi, lo: WideChar;
+  begin
+    while (aUtf8Count > 0) and (aUtf16Count > 0) do
+    begin
+      code := Unicode.Utf8ToCodepoint(aUtf8, aUtf8Count);
+
+      if code > $ffff then
+      begin
+        if aUtf16Count = 1 then
+          raise EMoreData.Create('%s requires a surrogate pair but only 1 widechar remains available in the destination buffer', [Unicode.Ref(code)]);
 
         Unicode.CodepointToSurrogates(code, hi, lo);
 
